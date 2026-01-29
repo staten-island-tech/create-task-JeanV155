45d1d43c8e8cc89013f718ffb99b48f16ad7acd3
JeanV155 <carlosgutierrez203074@gmail.com>
2026-01-29 12:31:52 -0500

    hjhjhjhjh

--- create.js
+++ create.js
@@ -1,7 +1,26 @@
-let n = 0
+let PastGuessesArray = [];
 
-while(n = undefined) {
-    
+const GuessNumber = () => {
+  const n = Math.floor(Math.random() * 20) + 1;
+  let guess;
+
+while(n !== answer) {
+    n = number(prompt("guess a number any number"));
+
+    PastGuesses.push(n)
+
+    if (guess > n) {
+        console.log("answer to low");
+ } else if (guess < n)
+    console.log("answer to low ");
+    else {
+        console.log("just right boyo");
+        
 
 
+
+    }
+
+
+}
 }
